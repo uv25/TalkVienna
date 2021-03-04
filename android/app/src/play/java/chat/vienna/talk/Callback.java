bedaa962758bc3d856431e9826c8745169b9df8c
uv25 <=>
2021-03-04 17:10:53 +0530

    Android Pacakage name changed

---
+++ android/app/src/play/java/chat/vienna/talk/Callback.java
@@ -0,0 +1,11 @@
+package chat.vienna.talk;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Callback {
+    public void call(@Nullable Bundle bundle) {
+
+    }
+}
